1291e0aeccd705bac2cac347c6e62feb8ed05bd8
MarTorb <marius.torbjornsen@gmail.com>
2021-02-19 14:34:49 +0100

    Create datatable over vessels

---
+++ src/components/VesselDatatable.vue
@@ -0,0 +1,73 @@
+<template>
+  
+    <div class="container">
+
+        <div>
+
+        <DataTable :value="vessels" v-model:selection="selectedVessel" selectionMode="single" dataKey="id"
+            :filters="filters">
+            <template #header>
+                Vessels
+                <span class="p-input-icon-left">
+                    <i class="pi pi-search" />
+                    <InputText type="text" v-model="filters['name']" placeholder="Keyword Search" />
+                </span>
+            </template>
+    
+            <Column field="name" header="Name" sortable filterMatchMode="contains"></Column>
+
+        </DataTable>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+
+
+
+export default defineComponent({
+    
+    name: "VesselDataTable",
+
+    setup:  () => {
+
+        const vessels = ref([] as Vessel[])
+        const selectedVessel = ref({} as Vessel)
+        const filters = ref({
+
+            "name" : ""
+
+        });
+
+        onMounted(() => {
+
+            vessels.value = [
+                {"name": "test1", "id": 1} as Vessel,
+                {"name": "test2", "id": 2} as Vessel,
+                {"name": "test3", "id": 3} as Vessel,
+                {"name": "test4", "id": 4} as Vessel,
+                {"name": "test5", "id": 5} as Vessel
+            ]
+
+        });
+
+        return {vessels, selectedVessel, filters};
+
+    }
+
+});
+
+interface Vessel {
+
+    name: string;
+    id: number;
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/VesselOverview.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <VesselDatatable/>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VesselDatatable from '@/VesselDatatable.vue';
+
+export default defineComponent({
+  components: {
+    VesselDatatable,
+  },
+});
+</script>
