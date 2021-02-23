68571a676617eda935f740600f4932a190ad16e2
mokkalokka <michael.larsen90@gmail.com>
2021-02-23 12:19:12 +0100

    Setting page alert

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,6 +3,9 @@
     <div v-if="saved" class="alert alert-primary" role="alert">
       Settings saved
     </div>
+    <div v-if="reverted" class="alert alert-danger" role="alert">
+      Settings reverted to default
+    </div>
     <h1 class="m-3">Your settings</h1>
     <DataTable :value="settings" editMode="cell" class="editable-cells-table">
       <Column field="description" header="Description"></Column>
@@ -10,10 +13,14 @@
       <Column field="value" header="Value">
         <template #editor="slotProps">
           <InputText v-model="slotProps.data.value" />
+          <p>{{slotProps.data.value}}</p>
         </template>
       </Column>
     </DataTable>
     <button class="btn btn-primary m-3" @click="saved = true">Save</button>
+    <button class="btn btn-danger m-3" @click="setDefault">
+      Revert to default
+    </button>
   </div>
 </template>
 
@@ -23,16 +30,31 @@ import { defineComponent, reactive, ref } from "vue";
 export default defineComponent({
   name: "Settings",
   setup() {
-    const settings = reactive([
+    let settings = reactive([
       { description: "Color theme", value: "Light" },
       { description: "Default vessel", value: "None" },
       { description: "Default number of datapoints per hour", value: "60" },
     ]);
+
+    const defaultSettings = reactive([
+      { description: "Color theme", value: "Light" },
+      { description: "Default vessel", value: "None" },
+      { description: "Default number of datapoints per hour", value: "60" },
+    ]);
+
     const saved = ref(false);
+    const reverted = ref(false);
+
+    const setDefault = () => {
+      reverted.value = true;
+      settings = { ...defaultSettings };
+    };
 
     return {
       settings,
       saved,
+      reverted,
+      setDefault,
     };
   },
 });
