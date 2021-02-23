23247f1fc1c0682ebb6d84eb318a616844b04e67
janloe <janloe@stud.ntnu.no>
2021-02-23 10:31:08 +0100

    Add primevue flexbox

--- src/components/VesselDatatable.vue
+++ src/components/VesselDatatable.vue
@@ -1,94 +1,87 @@
 <template>
-  
-    <div class="container">
+  <div class="container">
+    <div class="p-md-12 p-p-3 card">
+      <DataTable
+        :value="vessels"
+        v-model:selection="selectedVessel"
+        selectionMode="single"
+        dataKey="id"
+        :filters="filters"
+        @row-select="onRowSelect"
+      >
+        <template #header>
 
-        <div class="card">
+            Vessels
 
-        <DataTable :value="vessels" v-model:selection="selectedVessel" selectionMode="single" dataKey="id"
-            :filters="filters" @row-select="onRowSelect">
 
-            
-            <template #header>
-                
-                Vessels
+          <div class="p-d-flex p-jc-end">
+            <span class="p-input-icon-left">
+              <i class="pi pi-search" />
+              <InputText
+                type="text"
+                v-model="filters['name']"
+                placeholder="Vessel name"
+              />
+            </span>
+          </div>
+        </template>
 
-                
-                    <span class="p-input-icon-left">
-                        <i class="pi pi-search" />
-                        <InputText type="text" v-model="filters['name']" placeholder="Vessel name" />
-                    </span>
-
-            </template>
-    
-            <Column field="name" header="Name" sortable filterMatchMode="contains"></Column>
-
-        </DataTable>
-        </div>
+        <Column
+          field="name"
+          header="Name"
+          sortable
+          filterMatchMode="contains"
+        ></Column>
+      </DataTable>
     </div>
-
+  </div>
 </template>
 
 <script lang="ts">
 import router from "@/router";
 import { defineComponent, onMounted, ref } from "vue";
 
-
-
 export default defineComponent({
-    
-    name: "VesselDataTable",
+  name: "VesselDataTable",
 
-    setup:  () => {
+  setup: () => {
+    const vessels = ref([] as Vessel[]);
+    const selectedVessel = ref({} as Vessel);
+    const filters = ref({
+      name: "",
+    });
 
-        const vessels = ref([] as Vessel[])
-        const selectedVessel = ref({} as Vessel)
-        const filters = ref({
+    onMounted(() => {
+      vessels.value = [
+        { name: "test1", id: 1 } as Vessel,
+        { name: "test2", id: 2 } as Vessel,
+        { name: "test3", id: 3 } as Vessel,
+        { name: "test4", id: 4 } as Vessel,
+        { name: "test5", id: 5 } as Vessel,
+      ];
+    });
 
-            "name" : ""
-
-        });
-
-        onMounted(() => {
-
-            vessels.value = [
-                {"name": "test1", "id": 1} as Vessel,
-                {"name": "test2", "id": 2} as Vessel,
-                {"name": "test3", "id": 3} as Vessel,
-                {"name": "test4", "id": 4} as Vessel,
-                {"name": "test5", "id": 5} as Vessel
-            ]
-        });
-
-       function onRowSelect() {
-            router.push({name: "About", params: {id: selectedVessel.value.id}})
-        }
-
-        return {vessels, selectedVessel, filters, onRowSelect };
-
+    function onRowSelect() {
+      router.push({ name: "About", params: { id: selectedVessel.value.id } });
     }
 
+    return { vessels, selectedVessel, filters, onRowSelect };
+  },
 });
 
 interface Vessel {
-
-    name: string;
-    id: number;
-
+  name: string;
+  id: number;
 }
-
 </script>
 
 <style lang="scss">
-
 .card {
-    margin: 20px;
+  margin: 20px;
 }
 
 .search {
-    text-align: right;
-    display: inline-block;
+  text-align: right;
+  display: inline-block;
 }
-
-
-
 </style>
