f82dfcbcf4061d211a9ae08fd9c9356a4131a7cf
mokkalokka <michael.larsen90@gmail.com>
2021-02-23 10:01:19 +0100

    Created settings page

---
+++ src/views/Settings.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-if="saved" class="alert alert-primary" role="alert">
+      Settings saved
+    </div>
+    <h1 class="m-3">Your settings</h1>
+    <DataTable :value="settings" editMode="cell" class="editable-cells-table">
+      <Column field="description" header="Description"></Column>
+
+      <Column field="value" header="Value">
+        <template #editor="slotProps">
+          <InputText v-model="slotProps.data.value" />
+        </template>
+      </Column>
+    </DataTable>
+    <button class="btn btn-primary m-3" @click="saved = true">Save</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from "vue";
+
+export default defineComponent({
+  name: "Settings",
+  setup() {
+    const settings = reactive([
+      { description: "Color theme", value: "Light" },
+      { description: "Default vessel", value: "None" },
+      { description: "Default number of datapoints per hour", value: "60" },
+    ]);
+    const saved = ref(false);
+
+    return {
+      settings,
+      saved,
+    };
+  },
+});
+</script>
+
+<style>
+</style>
