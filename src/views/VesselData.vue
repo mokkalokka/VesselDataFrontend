809bb642fbd6059acd91879b06ebf3e534fce271
janloe <janloe@stud.ntnu.no>
2021-02-24 12:13:59 +0100

    Add responsiveness and filtering

--- src/views/VesselData.vue
+++ src/views/VesselData.vue
@@ -1,44 +1,36 @@
 <template>
-  <div class="p-md-12 p-p-3 card">
-    <Card>
-      <template #header>
-        <h1><i class="pi pi-compass"></i></h1>
-      </template>
-      <template #title> Sensors </template>
-      <template #content>
-        <Accordion v-model:activeIndex="active">
-          <AccordionTab header="Sensor table">
-            <!-- SENSOR TABLE HERE -->
-            <SensorTable
-              :sensorNames="sensorNames"
-              :selectedSensors="selectedSensors"
-            />
-          </AccordionTab>
-          <AccordionTab :disabled="!showSensorData" header="Sensor Data">
-            <!-- GRAPHS HERE -->
-            <div v-if="showSensorData">
-              <div v-for="s of selectedSensors" :key="s.id">
-                <p>{{ s.sensorName }}</p>
-                <line-graph :sensorName="s.sensorName" :sensorId="s.id" />
-              </div>
-            </div>
-          </AccordionTab>
-        </Accordion>
-      </template>
-      <template #footer>
+  <div class="card my-4">
+    <Accordion v-model:activeIndex="active">
+      <AccordionTab header="Sensor table">
+        <!-- SENSOR TABLE HERE -->
+        <SensorTable
+          :sensorNames="sensorNames"
+          :selectedSensors="selectedSensors"
+        />
+      </AccordionTab>
+      <AccordionTab :disabled="!showSensorData" header="Sensor Data">
+        <!-- GRAPHS HERE -->
+        <div v-if="showSensorData">
+          <div v-for="s of selectedSensors" :key="s.id">
+            <p>{{ s.sensorName }}</p>
+            <line-graph :sensorName="s.sensorName" :sensorId="s.id" />
+          </div>
+        </div>
+      </AccordionTab>
+    </Accordion>
+      <div class="p-d-flex p-jc-center">
         <Button
           label="view selected data"
           icon="pi pi-table"
-          class="p-button-rounded p-button-info"
+          class="p-button-rounded p-button-info p-m-4"
           @click="sendSelected"
         />
-      </template>
-    </Card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watchEffect } from "vue";
+import { defineComponent, ref } from "vue";
 import { useSensorData } from "@/composables/useSensorData";
 import { useSelectedSensors } from "@/composables/useSelectedSensors";
 import LineGraph from "@/components/LineGraph.vue";
@@ -51,7 +43,6 @@ export default defineComponent({
     const selectedSensors = useSelectedSensors();
     const showSensorData = ref(false);
     const active = ref(0 as number);
-    
 
     const sendSelected = () => {
       showSensorData.value = true;
@@ -59,7 +50,6 @@ export default defineComponent({
       selectedSensors.value.map((s) => {
         ids.push(s["id"]);
         console.log(s["startTime"], s["endTime"]);
-        
       });
       /* sensordata.value = getSensorDataById(ids); */
       active.value = 1;
@@ -71,9 +61,13 @@ export default defineComponent({
 
     initialize();
 
-
-    
-    return { active, showSensorData, sensorNames, selectedSensors, sendSelected };
+    return {
+      active,
+      showSensorData,
+      sensorNames,
+      selectedSensors,
+      sendSelected,
+    };
   },
 });
 </script>
