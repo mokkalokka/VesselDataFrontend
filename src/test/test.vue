d244f6e54bb8c3225261cf1077ed7934d8c84411
janloe <janloe@stud.ntnu.no>
2021-05-13 10:38:47 +0200

    Add testing libraries, and write small test

---
+++ src/test/test.vue
@@ -0,0 +1,10 @@
+<script>
+import { mount } from '@vue/test-utils'
+import vesselDataTable from '../../src/views/VesselData.vue'
+
+test('renders a todo', () => {
+  const wrapper = mount(vesselDataTable)
+
+  expect(wrapper.filter({id: 1, name: "Titanic"})).toBe(false)
+})
+</script>
